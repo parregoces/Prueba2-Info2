e4bc57f6c5e7e038fc97201750eb2b9a7b4a6614
paulina.arregocesg@udea.edu.co <paulina.arregocesg@udea.edu.co>
2025-10-03 13:39:28 -0300

    CambiosConflicto

--- HolaMundo/main.cpp
+++ HolaMundo/main.cpp
@@ -6,8 +6,6 @@ int main()
 {
     cout << "Hello World!" << endl;
 
-    cout <<"Prueba de Github"<<endl;
-
     cout<<"Prueba en ramaroja"<<endl;
     cout<<"Prueba en ramaverde"<<endl;
     return 0;
