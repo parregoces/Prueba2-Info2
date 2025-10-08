bca131ec79cc62382ccc2277498170f9b44abd73
paulina.arregocesg@udea.edu.co <paulina.arregocesg@udea.edu.co>
2025-10-08 11:25:05 -0300

    Cambios en main

--- HolaMundo/main.cpp
+++ HolaMundo/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello World!" << endl;
 
-    cout<<"Prueba en ramaroja"<<endl;
+    cout<<"Cambios en la rama principal"<<endl;
     cout<<"Prueba en ramaverde"<<endl;
 
     return 0;
