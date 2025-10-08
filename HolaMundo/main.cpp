16dbe9e93a256bc32533a5984c92a9ec7be990a2
paulina.arregocesg@udea.edu.co <paulina.arregocesg@udea.edu.co>
2025-10-08 11:06:45 -0300

    Haciendo un cambio en ramaroja

--- HolaMundo/main.cpp
+++ HolaMundo/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello World!" << endl;
 
-    cout<<"Prueba en ramaroja"<<endl;
+    cout<<"Haciendo un cambio en ramaroja"<<endl;
     cout<<"Prueba en ramaverde"<<endl;
     return 0;
 }
